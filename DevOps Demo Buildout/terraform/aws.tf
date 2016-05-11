863241162e00a95e10976f836cd8ec5de08825db
Eric Santelices <digitalroadies@users.noreply.github.com>
2016-05-11 10:49:35 -0400

    Delete aws.tf

--- DevOps Demo Buildout/terraform/aws.tf
+++
@@ -1,8 +0,0 @@
-# AWS variables for Region & API Access Key
-# not required if using environment variables on workstation
-
-provider "aws" {
-	#access_key = "${var.aws_access_key_id}"
-	#secret_key = "${var.aws_secret_access_key}"
-	#region = "${var.aws_region}"
-}
